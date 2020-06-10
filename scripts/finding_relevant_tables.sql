ba8e2c7110ccf58f9ccaa87c039c511c73c4d3ce
AmandaPartlow <62255772+AmandaPartlow@users.noreply.github.com>
2020-06-10 14:35:39 -0500

    creating demographics dataframe

---
+++ scripts/finding_relevant_tables.sql
@@ -0,0 +1,25 @@
+/* Count of all the tables that contain one of the keywords (seems high)
+
+SELECT COUNT(DISTINCT table_id)
+FROM meta_field_descriptions
+WHERE field_full_name ILIKE '%race%'
+OR field_full_name ILIKE '%hispanic%'
+OR field_full_name ILIKE '%income%'
+OR field_full_name ILIKE '%poverty%'; */
+
+
+/* Comparing to total count (still seems like initial number is inflated)
+
+SELECT COUNT(DISTINCT table_id)
+FROM meta_field_descriptions;*/
+
+
+/* Taking a look at corresponding field names - inflated because it's including a lot of rows I dont want
+
+SELECT DISTINCT table_id, field_full_name
+FROM meta_field_descriptions
+WHERE field_full_name ILIKE '%race%'
+OR field_full_name ILIKE '%hispanic%'
+OR field_full_name ILIKE '%income%'
+OR field_full_name ILIKE '%poverty%'; */
+
