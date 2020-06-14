d77acfc8f54c5ebc1e017ebf52664fbca3b65e1b
AmandaPartlow <62255772+AmandaPartlow@users.noreply.github.com>
2020-06-13 22:21:18 -0500

    put finishing touches on hh_income and race csvs; onto per-cap csv

--- scripts/finding_relevant_tables.sql
+++ scripts/finding_relevant_tables.sql
@@ -14,12 +14,10 @@ SELECT COUNT(DISTINCT table_id)
 FROM meta_field_descriptions;*/
 
 
-/* Taking a look at corresponding field names - inflated because it's including a lot of rows I dont want
+/* Taking a look at corresponding field names - inflated because it's including a lot of rows I dont want */
 
 SELECT DISTINCT table_id, field_full_name
 FROM meta_field_descriptions
-WHERE field_full_name ILIKE '%race%'
-OR field_full_name ILIKE '%hispanic%'
-OR field_full_name ILIKE '%income%'
-OR field_full_name ILIKE '%poverty%'; */
+WHERE field_full_name ILIKE 'PER CAPITA INCOME IN THE PAST 12 MONTHS%';
+
 
